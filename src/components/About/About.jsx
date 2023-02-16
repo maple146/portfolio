ed41dc5d9c98ca0b9b1654db967aa6d22fbbe9f8
Luciano Alcaraz <LGA-dev@hotmail.com>
2023-02-16 00:06:31 -0300

    Add basic site layout

---
+++ src/components/About/About.jsx
@@ -0,0 +1,11 @@
+import { Grid } from "@mui/material";
+
+const About = () => {
+    return (
+        <Grid>
+            About!
+        </Grid>
+    );
+}
+ 
+export default About;
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import { Grid } from "@mui/material";
+
+const Projects = () => {
+    return (
+        <Grid>
+            Projects!
+        </Grid>
+    );
+}
+ 
+export default Projects;
