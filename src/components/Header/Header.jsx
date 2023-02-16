651d4fe1dea925ea1823768b79fc32aabf9eb00e
Luciano Alcaraz <LGA-dev@hotmail.com>
2023-02-16 01:23:55 -0300

    Update Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,26 @@
-import { Box } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
+import CustomButton from "../CustomButton/CustomButton";
+import styles from './Header.module.css'
 
 const Header = () => {
     return (
-        <Box>
-            Header!
+        <Box className={styles.headerBackground}>
+            <Container className={styles.headerContainer}>
+                <Box className={styles.headerTextContent}>
+                    <Typography variant="h2" component="h2">
+                        Luciano Alcaraz
+                    </Typography>
+                    <Typography variant="h1" component="h1">
+                        Desarrollador Front-end
+                    </Typography>
+                </Box>
+                <Box>
+                    <CustomButton text={'LinkedIn'}/>
+                    <CustomButton text={'Github'}/>
+                </Box>
+            </Container>
         </Box>
     );
 }
- 
+
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <Header />
@@ -34,7 +33,6 @@ export default function Home() {
             <Footer />
           </Grid>
         </Grid>
-      </Container>
     </>
   )
 }
