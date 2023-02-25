ed1ea57af4da30c3acac6678f3a39566802b5e32
Luciano Alcaraz <LGA-dev@hotmail.com>
2023-02-25 18:17:51 -0300

    Add Projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,30 @@
-import { Grid } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
+import ProjectCard from "../ProjectCard/ProjectCard";
+import CustomTitle from "../CustomTitle/CustomTitle";
+import styles from './Projects.module.css'
 
 const Projects = () => {
     return (
-        <Grid>
-            Projects!
-        </Grid>
+        <Box  className={styles.projects}>
+            <Container>
+                <Grid
+                    container
+                    justifyContent='center'
+                    spacing={4}
+                >
+                    <Grid item xs={12} display='flex' justifyContent='center'>
+                        <CustomTitle
+                            text='Proyectos'
+                            variant='h3'
+                            component='h3'
+                        />
+                    </Grid>
+                    <Grid container item xs={12}>
+                      <ProjectCard />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
     );
 }
  
