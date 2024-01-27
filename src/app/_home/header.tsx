0f7f52e9ee015587ba9738bb2486c934fa68807e
Luciano Alcaraz <LGA-dev@hotmail.com>
2024-01-27 18:46:10 -0300

    Update button component, Add button component to header

--- src/app/_home/header.tsx
+++ src/app/_home/header.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <div className="flex flex-col bg-main-900">
-      <div className="container">
+    <div className="bg-main-900">
+      <div className="container flex flex-col items-center">
         <p className="text-sm">Text SM</p>
         <p className="text-base">Text BASE</p>
         <p className="text-lg">Text LG</p>
@@ -9,6 +12,14 @@ export default function Header() {
         <p className="text-xl-2">Text XL-2</p>
         <p className="text-xl-3">Text XL-3</p>
         <p className="text-xl-4">Text XL-4</p>
+        <div className="flex gap-4">
+          <Link href="https://github.com/maple146">
+            <Button>Github</Button>
+          </Link>
+          <Link href="https://www.linkedin.com/in/lucianoalcaraz/">
+            <Button>Linkedin</Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
