a4d88090fea32b4547d7574a4b903d4ceaaae030
Luciano Alcaraz <LGA-dev@hotmail.com>
2024-01-27 22:35:26 -0300

    Add content to Projects section

--- src/app/_home/projects.tsx
+++ src/app/_home/projects.tsx
@@ -1,8 +1,71 @@
+import Title from "@/components/title";
+
 export default function Projects() {
-    return (
-      <div className="container">
-        <h1 className="text-2xl lg:text-8xl">Projects</h1>
+  return (
+    <div className="container grid grid-cols-3 py-8 gap-4 lg:gap-8">
+      <div className="col-span-full flex justify-center mb-2 lg:mb-4">
+        <Title text={"Proyectos"} />
       </div>
-    );
-  }
-  
+      <div className="col-span-full lg:col-start-1 lg:col-end-2 flex flex-col">
+        <p className="text-base lg:text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          luctus, ligula eget sagittis venenatis, sem nisl condimentum metus,
+          eget cursus sem tellus vel nisl. Suspendisse efficitur tempor libero
+          eu pharetra. Maecenas ullamcorper eros eros. Quisque mollis enim id
+          tellus laoreet ultrices. Phasellus blandit eget nisl tristique
+          aliquet. Quisque a risus nisl.
+        </p>
+      </div>
+      <div className="col-span-full lg:col-start-2 lg:col-end-3 flex flex-col">
+        <p className="text-base lg:text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          luctus, ligula eget sagittis venenatis, sem nisl condimentum metus,
+          eget cursus sem tellus vel nisl. Suspendisse efficitur tempor libero
+          eu pharetra. Maecenas ullamcorper eros eros. Quisque mollis enim id
+          tellus laoreet ultrices. Phasellus blandit eget nisl tristique
+          aliquet. Quisque a risus nisl.
+        </p>
+      </div>
+      <div className="col-span-full lg:col-start-3 lg:col-end-4 flex flex-col">
+        <p className="text-base lg:text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          luctus, ligula eget sagittis venenatis, sem nisl condimentum metus,
+          eget cursus sem tellus vel nisl. Suspendisse efficitur tempor libero
+          eu pharetra. Maecenas ullamcorper eros eros. Quisque mollis enim id
+          tellus laoreet ultrices. Phasellus blandit eget nisl tristique
+          aliquet. Quisque a risus nisl.
+        </p>
+      </div>
+      <div className="col-span-full lg:col-start-1 lg:col-end-2 flex flex-col">
+        <p className="text-base lg:text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          luctus, ligula eget sagittis venenatis, sem nisl condimentum metus,
+          eget cursus sem tellus vel nisl. Suspendisse efficitur tempor libero
+          eu pharetra. Maecenas ullamcorper eros eros. Quisque mollis enim id
+          tellus laoreet ultrices. Phasellus blandit eget nisl tristique
+          aliquet. Quisque a risus nisl.
+        </p>
+      </div>
+      <div className="col-span-full lg:col-start-2 lg:col-end-3 flex flex-col">
+        <p className="text-base lg:text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          luctus, ligula eget sagittis venenatis, sem nisl condimentum metus,
+          eget cursus sem tellus vel nisl. Suspendisse efficitur tempor libero
+          eu pharetra. Maecenas ullamcorper eros eros. Quisque mollis enim id
+          tellus laoreet ultrices. Phasellus blandit eget nisl tristique
+          aliquet. Quisque a risus nisl.
+        </p>
+      </div>
+      <div className="col-span-full lg:col-start-3 lg:col-end-4 flex flex-col pb-4">
+        <p className="text-base lg:text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          luctus, ligula eget sagittis venenatis, sem nisl condimentum metus,
+          eget cursus sem tellus vel nisl. Suspendisse efficitur tempor libero
+          eu pharetra. Maecenas ullamcorper eros eros. Quisque mollis enim id
+          tellus laoreet ultrices. Phasellus blandit eget nisl tristique
+          aliquet. Quisque a risus nisl.
+        </p>
+      </div>
+    </div>
+  );
+}
