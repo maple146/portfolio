0f9e8d432207e22e021f2f501eef6cfe35105bf9
Luciano Alcaraz <LGA-dev@hotmail.com>
2024-01-27 23:02:37 -0300

    Add Contact section component

--- src/app/_home/contact.tsx
+++ src/app/_home/contact.tsx
@@ -1,8 +1,31 @@
+import Title from "@/components/title";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Alert, AlertDescription } from "@/components/ui/alert";
+
 export default function Contact() {
   return (
-    <div className="flex flex-col bg-main-900">
-      <div className="container">
-        <h1 className="text-2xl lg:text-8xl">Contact</h1>
+    <div className="bg-main-900 h-screen lg:h-[480px]">
+      <div className="container flex flex-col items-center justify-center h-full text-center">
+        <Title text={"Contacto"} />
+
+        <div className="my-4 lg:my-8 max-w-[700px]">
+          <Alert>
+            <AlertDescription>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque dui lectus, consectetur nec elementum eget.
+            </AlertDescription>
+          </Alert>
+        </div>
+
+        <div className="flex gap-4 flex-col lg:flex-row">
+          <Link href="LGA-dev@hotmail.com">
+            <Button>Email</Button>
+          </Link>
+          <Link href="https://www.linkedin.com/in/lucianoalcaraz/">
+            <Button>Linkedin</Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
