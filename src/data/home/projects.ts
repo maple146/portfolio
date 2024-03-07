219f194656e814ed431acc97c77b823f869122c6
Luciano Alcaraz <LGA-dev@hotmail.com>
2024-03-07 16:53:42 -0300

    Update projects data

--- src/data/home/projects.ts
+++ src/data/home/projects.ts
@@ -1,37 +1,67 @@
 export const PROJECTS_DATA = {
   home: {
-    sectionLink: '/proyectos',
+    sectionLink: "/proyectos",
     title: "Proyectos",
     projects: [
       {
         title: "Telecom Institucional",
-        subtitle: "Sitio institucional",
+        subtitle: "Sitio institucional para Telecom",
         description:
-          "Este sitio cuenta con toda la información sobre la empresa, así como sus servicios, noticias, eventos, inscripciones, newsletters, podcasts y otros aspectos relacionados.",
+          "Este fue el primer proyecto en el que trabajé de manera profesional. Durante su desarrollo, nos esforzamos por atender las necesidades de la empresa durante su proceso de unificación marcaría, asegurándonos de presentar toda su información de manera completa.",
         image: "/institucional.png",
         imageAlt: "Screenshot de la pagina Telecom Institucional",
         externalUrl: "https://institucional.telecom.com.ar",
-        internalUrl: "/proyectos/telecom-institucional",
-      },
-      {
-        title: "Savencia",
-        subtitle: "Plataforma e-commerce B2B",
-        description:
-          "Este proyecto está orientado a clientes distribuidores. Ofrece funcionalidades centradas en la venta a negocios, como búsquedas por código, compras por bultos, repetición de pedidos y visualización de comprobantes.",
-        image: "/savencia.png",
-        imageAlt: "Screenshot de la pagina Savencia E-Commerce",
-        externalUrl: "https://shouganai.estoes.dev",
-        internalUrl: "/proyectos/savencia",
+        // internalUrl: "/proyectos/telecom-institucional",
       },
       {
         title: "Vieníssima",
-        subtitle: "Landing de productos",
+        subtitle: "Landing de productos para Mafrig",
         description:
-          "Relanzamos el sitio con el objetivo de reflejar el nuevo diseño de las salchichas y actualizar el contenido según la ley de sellos. El sitio cuenta con muchas animaciones que lo hace muy dinámico y fluido durante la navegación.",
+          "Relanzamos el sitio con el objetivo de reflejar el nuevo diseño de las salchichas y actualizar el contenido según la ley de sellos. Contiene numerosas transiciones y animaciones que ofrecen una navegación muy fluida y dinámica.",
         image: "/vienissima.png",
         imageAlt: "Screenshot de la pagina Vieníssima",
         externalUrl: "https://www.vienissima.ar/",
-        internalUrl: "/proyectos/vienissima",
+        // internalUrl: "/proyectos/vienissima",
+      },
+      {
+        title: "DAP Latam",
+        subtitle: "Landing de productos para RPM",
+        description:
+          "Creamos el sitio con la idea de poder mostrarle a los usuarios una variedad de productos y facilitarles su uso de manera eficiente mediante videotutoriales y tablas comparativas. Además, cuenta con un blog donde se publican guías específicas tanto para productos como para solucionar problemas particulares.",
+        image: "/daplatam.png",
+        imageAlt: "Screenshot de la pagina DAP Latam",
+        externalUrl: "https://dap-latam.com/",
+        // internalUrl: "/proyectos/",
+      },
+      {
+        title: "Mas mecanico",
+        subtitle: "Sitio de e-learning para ZF",
+        description:
+          "Relanzamos el sitio actualizando la tecnología de fondo, pero manteniendo la esencia del diseño original. Cuenta con diversos cursos de mecánica, con la opción de tomar exámenes en línea y recibir un certificado al aprobarlo.",
+        image: "/masmecanico.png",
+        imageAlt: "Screenshot de la pagina Mas Mecanico",
+        externalUrl: "https://masmecanico.com.mx/",
+        // internalUrl: "/proyectos/",
+      },
+      {
+        title: "Alleata",
+        subtitle: "Landing del producto para Moretti",
+        description:
+          "Creamos el sitio con el objetivo de dar a conocer la terminal creada por la fintech. Cuenta con secciones que muestran los beneficios de usar el producto, los planes de suscripción disponibles, videotutoriales de cómo usarla y un formulario donde se debe cargar toda la documentación necesaria para adquirir la terminal.",
+        image: "/alleata.png",
+        imageAlt: "Screenshot de la pagina Alleata",
+        externalUrl: "https://alleata.estoes.dev/home",
+        // internalUrl: "/proyectos/",
+      },
+      {
+        title: "Personal",
+        subtitle: "Landings de servicios de Personal",
+        description:
+          "Colaboré con el equipo de Conexión Afuera en Telecom durante 8 meses, centrándonos en la migración de varias landings a Next.js. Durante este tiempo, no solo actualizamos la tecnología, sino que también renovamos el diseño y el estilo de dichas páginas.",
+        image: "/paginaspersonal.png",
+        imageAlt: "Screenshot de las paginas de Personal",
+        externalUrl: "",
+        // internalUrl: "/proyectos/",
       },
     ],
   },
