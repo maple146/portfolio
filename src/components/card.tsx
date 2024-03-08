3a015c8dbd5b72f5f1fa637c46c78cd0afba413d
Luciano Alcaraz <LGA-dev@hotmail.com>
2024-03-07 21:55:59 -0300

    Fix card elements position

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -21,16 +21,17 @@ const Card = ({
 }:
 CardTypes) => {
   return (
-    <div className="bg-main-700 rounded-lg shadow overflow-hidden">
-      <div className="relative h-[200px] w-full">
+    // Agregar 'shadow' cuando se implemente el light mode
+    <div className="bg-main-700 rounded-lg overflow-hidden h-full flex flex-col">
+      <div className="relative min-h-[200px] w-full">
         <Image fill src={image} alt={imageAlt} className="object-cover object-top"/>
       </div>
-      <div className="p-4 flex flex-col justify-between min-h-[320px]">
-        <div>
+      <div className="p-4 flex flex-col justify-between h-full">
+        <div className="pb-8">
           <h5 className="text-lg font-bold text-white">{title}</h5>
           <h6 className="text-sm font-bold text-white">{subtitle}</h6>
           <hr className="my-2"></hr>
-          <p className="text-sm font-normal text-white pb-8">{description}</p>
+          <p className="text-sm font-normal text-white">{description}</p>
         </div>
         <div className="flex gap-4">
           <Link href={externalUrl} rel="noopener noreferrer" target="_blank">
