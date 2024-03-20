53fa80c0c9c9010f414b28a821eefd5b4de731af
Luciano Alcaraz <LGA-dev@hotmail.com>
2024-03-20 05:23:34 -0300

    Add svg icon to CustomLink component

--- src/components/custom-link.tsx
+++ src/components/custom-link.tsx
@@ -1,3 +1,4 @@
+import { ExternalLinkIcon } from "lucide-react";
 import Link from "next/link";
 
 export interface CustomLinkTypes {
@@ -8,12 +9,10 @@ export interface CustomLinkTypes {
 
 const CustomLink = ({ url, text, size }: CustomLinkTypes) => {
   return (
-    <Link href={url} rel="noopener noreferrer" target="_blank">
+    <Link href={url} rel="noopener noreferrer" target="_blank" className="text-main-100 flex items-center hover:underline">
+      <ExternalLinkIcon />
       <p
-        className={`text-main-100 ${
-          // Refactorizar usando cva
-          size === "sm" ? "text-sm" : "text-base lg:text-lg"
-        }  hover:underline`}
+        className={`${size === "sm" ? "text-sm" : "text-base lg:text-lg"} ml-2`}
       >
         {text}
       </p>
