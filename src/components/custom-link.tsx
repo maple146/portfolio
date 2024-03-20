1a34868d0ff7ac6ab784a0188eec33f0f3cdb543
Luciano Alcaraz <LGA-dev@hotmail.com>
2024-03-20 04:57:13 -0300

    Add small version for CustomLink component

--- src/components/custom-link.tsx
+++ src/components/custom-link.tsx
@@ -3,12 +3,18 @@ import Link from "next/link";
 export interface CustomLinkTypes {
   url: string;
   text: string;
+  size: string;
 }
 
-const CustomLink = ({ url, text }: CustomLinkTypes) => {
+const CustomLink = ({ url, text, size }: CustomLinkTypes) => {
   return (
     <Link href={url} rel="noopener noreferrer" target="_blank">
-      <p className="text-main-100 text-base lg:text-lg hover:underline">
+      <p
+        className={`text-main-100 ${
+          // Refactorizar usando cva
+          size === "sm" ? "text-sm" : "text-base lg:text-lg"
+        }  hover:underline`}
+      >
         {text}
       </p>
     </Link>
