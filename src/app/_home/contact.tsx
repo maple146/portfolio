9f58426c436d5e725b4be13f97d4c4acbd4f5cb2
Luciano Alcaraz <LGA-dev@hotmail.com>
2024-07-31 20:11:38 -0300

    Update contact card style

--- src/app/_home/contact.tsx
+++ src/app/_home/contact.tsx
@@ -1,6 +1,5 @@
 import Title from "@/components/title";
 import { Button } from "@/components/ui/button";
-import { Alert, AlertDescription } from "@/components/ui/alert";
 import { ContactTypes } from "@/types/contact.types";
 
 export default function Contact({ title, text, buttons }: ContactTypes) {
@@ -8,12 +7,13 @@ export default function Contact({ title, text, buttons }: ContactTypes) {
     <div className="h-[480px]">
       <div className="container flex flex-col items-center justify-center h-full text-center">
         <Title text={title} />
-        <div className="my-4 lg:my-8 max-w-[700px]">
-          <Alert>
-            <AlertDescription>
-              {text}
-            </AlertDescription>
-          </Alert>
+        <div className="
+          max-w-[700px] relative w-full rounded-lg p-4 lg:px-12 lg:py-8 my-4 lg:my-8
+          border-2 border-[rgba(46,187,144,0.3)] bg-gradient-to-tl from-[rgba(46,187,144,0.15)] to-[rgba(0,0,0,0.05)]
+        ">
+          <p className="text-base lg:text-base">
+            {text}
+          </p>
         </div>
         <div className="flex gap-4 flex-col md:flex-row">
           {
