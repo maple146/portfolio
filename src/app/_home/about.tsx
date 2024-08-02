b89df645765458a12f1a017f653ca5403c992c34
Luciano Alcaraz <LGA-dev@hotmail.com>
2024-08-02 14:01:23 -0300

    Fix skills section mobile styles

--- src/app/_home/about.tsx
+++ src/app/_home/about.tsx
@@ -49,7 +49,7 @@ export default function About({ title, sections }: AboutTypes) {
               }
               {
                 section.skills && (
-                  <div className="grid grid-cols-2 gap-2 md:gap-4">
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
                     {
                       section.skills.map((skill) => (
                         <div key={skill.category} className="flex flex-col gap-2">
